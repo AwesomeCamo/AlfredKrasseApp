23abd7b26350c460d8ee65d6da125f9d2893866c
Dennis F <dede01.df@gmail.com>
2021-12-20 19:48:33 +0100

    alle knöpfe umgeändert (Layout auch)

--- main.py
+++ main.py
@@ -2,35 +2,63 @@ from tkinter import *
 
 root = Tk()
 root.title('Alfred Krasse App')
-root.iconbitmap("./recources/images/HomeIcon.ico")
-root.geometry("400x700")
+root.iconbitmap("./resources/images/HomeIcon.ico")
+root.geometry("1920x1080")
+rootHeight = root.winfo_height()
+rootWidth = root.winfo_width()
 root['background'] = '#FFFEF6'
 
 def my_click():
-    myLabel.grid_forget() #vergisst den grid, kann aber wieder neu aufgerufen werden durch "myLabel.grid()"
-    myButton.grid_forget()
-    myLabel2.grid(row=0, column=0)
-    myButton2.grid(row=1,column=0)
+    #myButton.place_forget()          #vergisst den grid, kann aber wieder neu aufgerufen werden durch "myButton.grid()"
+    #myButton2.place(relx=0.5,rely=0.5,anchor=CENTER)
     return
 
 def my_clickback():
-    myLabel2.grid_forget()
-    myButton2.grid_forget()
-    myLabel.grid(row=0,column=0)
-    myButton.grid(row=3, column=0)
+    #myButton2.place_forget()
+    #myButton.place(relx=0.5,rely=0.5,anchor=CENTER)
     return
 
-myLabel = Label(root, text="Hello World")
-myLabel2 = Label(root, text="For Dennis <3")
-myButton = Button(root, text="click me", command=my_click)
-myButton2 = Button(root, text="Click me to go back", command=my_clickback)
+#myButton1 = Button(root, text="Unterricht",command=my_click, bg="#2d2d2d",fg="#e4bc1f",width=30,height=10 )
+#myButton2 = Button(root, text="Hauptmenü",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=30,height=10)
+#myButton3 = Button(root, text="Schule",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=30,height=10)
+#myButton4 = Button(root, text="Persönliche Daten",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=30,height=10)
+#myButton5 = Button(root, text="Einstellungen",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=30,height=10)
+#myButton6 = Button(root, text="Hauptmenü",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=30,height=10)
 
+#myButton.place(relx=0.25,rely=0.25,anchor=CENTER)
+#myButton2.place(relx=0.5,rely=0.5,anchor=CENTER)
+#myButton3.place(relx=0.5,rely=0.5,anchor=CENTER)
+#myButton4.place(relx=0.75,rely=0.75,anchor=CENTER)
+#myButton5.place(relx=0.5,rely=0.5,anchor=CENTER)
+#myButton6.(relx=0.5,rely=0.5,anchor=CENTER)
+#myButton2.place_forget()
+#myButton6.place_forget()
 
+frame = Frame(root)
+frame.pack()
 
+Topframe = Frame(root)
+Topframe.pack(side = TOP)
 
-myLabel.grid(row=0, column=0)
-#myLabel2.grid(row = 1, column = 0)
-myButton.grid(row=3, column=0)
-#myButton2.grid(row=4, column=0)
+myButton1 = Button(frame, text="Unterricht",command=my_click, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton1.pack(side = LEFT)
+
+myButton2 = Button(frame, text="Schule",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton2.pack(side = LEFT)
+
+myButton3 = Button(frame, text="Persönliche Daten",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton3.pack(side = LEFT)
+
+myButton4 = Button(frame, text="Einstellungen",command=my_clickback, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton4.pack(side = LEFT)
+
+Bottomframe = Frame(root)
+Bottomframe.pack(side = BOTTOM)
+
+myEvent = Label(text="Event", bg="#2d2d2d",fg="#e4bc1f",width=rootWidth //2,height=65)
+myEvent.pack(side= LEFT)
+
+myAusfall = Label(text="Ausfall/Vertretung", bg="#2d2d2d",fg="#e4bc1f",width=rootWidth //2,height=65)
+myAusfall.pack(side= RIGHT)
 
 root.mainloop()
