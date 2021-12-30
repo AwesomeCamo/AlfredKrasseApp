cbe789b3ebc00c43bebe19e52055554b262174f0
Dennis F <dede01.df@gmail.com>
2021-12-30 16:45:18 +0100

    main.py wurden alle Frames für jeden Button erstellt.

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from tkinter import *
-from pymongo import MongoClient
 
 seitenzähler = 0
-cluster = MongoClient("mongodb+srv://Dennis:MHhRui10mongodb@cluster0.aitqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = cluster["Alfred-Krasse-App"]
-collection1 = db["Lehrerkürzel"]
-collection2 = db["Unterricht"]
-collection3 = db["Ausfall"]
 root = Tk()
 root.title('Alfred Krasse App')                         #titel name
 root.iconbitmap("./resources/images/HomeIcon.ico")      #Bild (icon oben links)
@@ -14,26 +8,50 @@ rootHeight = root.winfo_screenheight()                        #die Höhe des Fen
 rootWidth = root.winfo_screenwidth()
 root.geometry("%dx%d+0+0"%(rootWidth,rootHeight))
 root['background'] = '#FFFEF6'                          #der Hintergrund
-db = cluster["Alfred-Krasse-App"]
-collection1 = db["Lehrerkürzel"]
 
-navigation = Frame(root)
-navigation.pack()
+parent_navigation = Frame(root,width=rootWidth,height=rootHeight//4)
+parent_navigation.grid()
 
-inhalt = Frame(root)
-inhalt.pack()
+unterricht_frame_navigation = Frame(parent_navigation,width=rootWidth//4,height=rootHeight//4)
+unterricht_frame_navigation.grid(row=0,column=0)
 
-frame_grid = Frame(inhalt)
-frame_grid.pack()
-frame_grid.pack_forget()
+schule_frame_navigation = Frame(parent_navigation,width=rootWidth//4,height=rootHeight//4)
+schule_frame_navigation.grid(row=0,column=1)
 
-frame_info = Frame(inhalt)
-frame_info.pack()
-frame_info.pack_forget()
+persönliche_daten_frame_navigation = Frame(parent_navigation,width=rootWidth//4,height=rootHeight//4)
+persönliche_daten_frame_navigation.grid(row=0,column=2)
 
-frame_info2 = Frame(inhalt)
-frame_info2.pack()
-frame_info2.pack_forget()
+hauptmenü_frame_navigation = Frame(parent_navigation,width=rootWidth//4,height=rootHeight//4)
+hauptmenü_frame_navigation.grid(row=0,column=3)
+
+frame_parent_bottom = Frame(root,width=rootWidth,height=rootHeight//2+rootHeight//4)
+frame_parent_bottom.grid(row=1)
+
+frame_inhalt_event = Frame(frame_parent_bottom,width=rootWidth//2,height=rootHeight//2+rootHeight//4,bg="#2d2d2d")
+frame_inhalt_event.grid(row=1,column=1)
+
+frame_inhalt_ausfall = Frame(frame_parent_bottom,width=rootWidth//2,height=rootHeight//2+rootHeight//4,bg="#2d2d2d")
+frame_inhalt_ausfall.grid(row=1,column=2)
+
+frame_inhalt_lehrerkürzel = Frame(frame_parent_bottom,width=rootWidth//2,height=rootHeight//2+rootHeight//4,bg="#2d2d2d")
+frame_inhalt_lehrerkürzel.grid(row=1,column=1)
+frame_inhalt_lehrerkürzel.grid_forget()
+
+frame_inhalt_lehrerkürzel_left = Frame(frame_parent_bottom,width=rootWidth//2,height=rootHeight//2+rootHeight//4,bg="#2d2d2d")
+frame_inhalt_lehrerkürzel_left.grid(row=1,column=1)
+frame_inhalt_lehrerkürzel_left.grid_forget()
+
+frame_inhalt_lehrerkürzel_right = Frame(frame_parent_bottom,width=rootWidth//2,height=rootHeight//2+rootHeight//4,bg="#2d2d2d")
+frame_inhalt_lehrerkürzel_right.grid(row=1,column=2)
+frame_inhalt_lehrerkürzel_right.grid_forget()
+
+frame_inhalt_über_schule = Frame(frame_parent_bottom,width=rootWidth//2,height=rootHeight//2+rootHeight//4,bg="#2d2d2d")
+frame_inhalt_über_schule.grid(row=1,column=2)
+frame_inhalt_über_schule.grid_forget()
+
+frame_grid = Frame(root)
+frame_grid.grid()
+frame_grid.grid_forget()
 
 class Information:
     def __init__(self,Name2,Position):
@@ -64,24 +82,49 @@ def sub_seitenzähler():
         seitenzähler -= 1
 
 def forget_for_lehrerkürzel():
-    myEvent.pack_forget()
-    myAusfall.pack_forget()
-    myLehrerkürzel.pack_forget()
-    myInformation.pack_forget()
-    frame_grid.pack_forget()
+    myEvent.place_forget()
+    myAusfall.place_forget()
+    myLehrerkürzel.place_forget()
+    myInformation.place_forget()
+    frame_grid.place_forget()
     buttonblack()
-    frame_info.pack_forget()
-    frame_info2.pack_forget()
+    for child in frame_inhalt_lehrerkürzel.winfo_children():
+        child.place_forget()
+
+    for child in frame_inhalt_über_schule.winfo_children():
+        child.place_forget()
+
+    for child in frame_inhalt_ausfall.winfo_children():
+        child.place_forget()
+
+    for child in frame_inhalt_event.winfo_children():
+        child.place_forget()
 
 def forgetinhalt():
-    myEvent.pack_forget()
-    myAusfall.pack_forget()
-    myLehrerkürzel.pack_forget()
-    myInformation.pack_forget()
-    frame_grid.pack_forget()
+    myEvent.place_forget()
+    myAusfall.place_forget()
+    myLehrerkürzel.place_forget()
+    myInformation.place_forget()
+    frame_inhalt_lehrerkürzel_right.grid_forget()
+    frame_inhalt_lehrerkürzel_left.grid_forget()
+    frame_grid.place_forget()
+    for child in frame_inhalt_lehrerkürzel.winfo_children():
+        child.place_forget()
+
+    for child in frame_inhalt_über_schule.winfo_children():
+        child.place_forget()
+
+    for child in frame_inhalt_ausfall.winfo_children():
+        child.place_forget()
+
+    for child in frame_inhalt_event.winfo_children():
+        child.place_forget()
+
+    frame_inhalt_ausfall.grid_forget()
+    frame_inhalt_event.grid_forget()
+    frame_inhalt_über_schule.grid_forget()
+    frame_inhalt_lehrerkürzel.grid_forget()
     buttonblack()
-    frame_info.pack_forget()
-    frame_info2.pack_forget()
     global seitenzähler
     seitenzähler = 0
 
@@ -97,14 +140,18 @@ def unterricht():                                       #Funktion erstellen
 
 def hauptmenü():
     forgetinhalt()
-    myEvent.pack(side=LEFT)
-    myAusfall.pack(side=LEFT)
+    frame_inhalt_event.grid(row=1,column=1)
+    frame_inhalt_ausfall.grid(row=1,column=2)
+    myEvent.place(x=0,y=0,width=rootWidth//2,height=rootHeight//2+rootHeight//4)
+    myAusfall.place(x=0,y=0,width=rootWidth//2,height=rootHeight//2+rootHeight//4)
     myButton4.config(bg="#7f7f7f")
 
 def schule():
     forgetinhalt()
-    myLehrerkürzel.pack(side=LEFT)
-    myInformation.pack(side=LEFT)
+    frame_inhalt_lehrerkürzel.grid(row=1,column=1)
+    frame_inhalt_über_schule.grid(row=1,column=2)
+    myLehrerkürzel.place(x=0,y=0,width=rootWidth//2,height=rootHeight//2+rootHeight//4)
+    myInformation.place(x=0,y=0,width=rootWidth//2,height=rootHeight//2+rootHeight//4)
     myButton2.config(bg="#7f7f7f")
 
 def persönliche_daten():
@@ -120,16 +167,14 @@ def ausfall():
 def lehrerkürzel(seite):
     forget_for_lehrerkürzel()
     myButton2.config(bg="#7f7f7f")
-    frame_grid.pack()
+    frame_grid.place()
     for child in frame_grid.winfo_children():
         child.destroy()
     i=0
-    label1 = Label(frame_grid, text="Kürzel", relief="solid", borderwidth="2", width=93, height=3,
-                   anchor=CENTER, bg="#2d2d2d", fg="#e4bc1f")
-    label2 = Label(frame_grid, text="Name", relief="solid", borderwidth="2", width=93, height=3, anchor=CENTER,
-                   bg="#2d2d2d", fg="#e4bc1f")
-    label3 = Label(frame_grid, text="Email", relief="solid", borderwidth="2", width=93, height=3, anchor=CENTER,
-                   bg="#2d2d2d", fg="#e4bc1f")
+    label1 = Label(frame_grid, text="Kürzel", relief="solid", borderwidth="2", width=93, height=3,anchor=CENTER, bg="#2d2d2d", fg="#e4bc1f")
+    label2 = Label(frame_grid, text="Name", relief="solid", borderwidth="2", width=93, height=3, anchor=CENTER,bg="#2d2d2d", fg="#e4bc1f")
+    label3 = Label(frame_grid, text="Email", relief="solid", borderwidth="2", width=93, height=3, anchor=CENTER,bg="#2d2d2d", fg="#e4bc1f")
+
     label1.grid(row=i, column=1, sticky=W)
     label2.grid(row=i, column=2, sticky=W)
     label3.grid(row=i, column=3, sticky=W)
@@ -147,9 +192,9 @@ def lehrerkürzel(seite):
 
 def über_die_Schule():
     forgetinhalt()
+    frame_inhalt_lehrerkürzel_right.grid(row=1,column=2)
+    frame_inhalt_lehrerkürzel_left.grid(row=1,column=1)
     myButton2.config(bg="#7f7f7f")
-    frame_info.pack()
-    frame_info2.pack()
     x=0
     Headline = Information("Name", "Position")
     Allmann = Information("Pete Allmann","Schulleiter")
@@ -157,31 +202,24 @@ def über_die_Schule():
     Mohr = Information("Christoph Mohr","Zweiter Stellvertreter")
     Klein = Information("Frau Klein","Sekretärin")
     Spada = Information("Frau Spada","Sekretärin")
-    daten = [Headline,Allmann,Schwamm,Mohr,Klein,Spada]
+    adress = Information("Lessingstraße 24, 76887 Bad Bergzabern","06343-9344-0")
+    daten = [Headline,Allmann,Schwamm,Mohr,Klein,Spada,adress]
     for name in daten:
         if x == 0:
-            label1 = Label(frame_info,text=name.Name2, relief="solid", borderwidth="2", width=140,height=5, anchor=CENTER, bg="#2d2d2d",fg="#e4bc1f")
+            label1 = Label(frame_inhalt_lehrerkürzel_left,text=name.Name2, relief="solid", borderwidth="2", anchor=CENTER, bg="#2d2d2d",fg="#e4bc1f")
             label1.config(font=("Monteserrat", 9,"bold italic"))
-            label2 = Label(frame_info,text=name.Position, relief="solid", borderwidth="2", width=140,height=5, anchor=CENTER, bg="#2d2d2d",fg="#e4bc1f")
+            label2 = Label(frame_inhalt_lehrerkürzel_right,text=name.Position, relief="solid", borderwidth="2",  anchor=CENTER, bg="#2d2d2d",fg="#e4bc1f")
             label2.config(font=("Monteserrat", 9,"bold italic"))
         else:
-            label1 = Label(frame_info, text=name.Name2, relief="solid", borderwidth="2", width=140,height=5, anchor=W, bg="#2d2d2d",fg="#e4bc1f")
+            label1 = Label(frame_inhalt_lehrerkürzel_left, text=name.Name2, relief="solid", borderwidth="2", anchor=W, bg="#2d2d2d",fg="#e4bc1f")
             label1.config(font=("Monteserrat", 9))
-            label2 = Label(frame_info, text=name.Position, relief="solid", borderwidth="2", width=140,height=5, anchor=W, bg="#2d2d2d",fg="#e4bc1f")
+            label2 = Label(frame_inhalt_lehrerkürzel_right, text=name.Position, relief="solid", borderwidth="2", anchor=W, bg="#2d2d2d",fg="#e4bc1f")
             label2.config(font=("Monteserrat", 9))
 
-        label1.grid(row=x, column=1, sticky=W)
-        label2.grid(row=x, column=2, sticky=W)
+        label1.place(y=x*(rootHeight-rootHeight//4)//8-1,height=(rootHeight-rootHeight//4)//7,width=rootWidth//2)
+        label2.place(y=x*(rootHeight-rootHeight//4)//8-1,height=(rootHeight-rootHeight//4)//7,width=rootWidth//2)
         x+=1
 
-    anschrift = Label(frame_info2,text="Lessingstraße 24, 76887 Bad Bergzabern", relief="solid", borderwidth="2",height=12, width=280, anchor=CENTER, bg="#2d2d2d",fg="#e4bc1f")
-    anschrift.config(font=("Monteserrat", 9))
-
-    telefon = Label(frame_info2,text="06343-9344-0", relief="solid", borderwidth="2", width=280,height=12, anchor=CENTER, bg="#2d2d2d",fg="#e4bc1f")
-    telefon.config(font=("Monteserrat", 9))
-
-    anschrift.grid(row=x,column=1,sticky=W)
-    telefon.grid(row=x+1,column=1,sticky=W)
 
 button_back = Button(frame_grid,text="Back",command=lambda:[sub_seitenzähler(),lehrerkürzel(seitenzähler*16)],bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
 button_back.grid(row=16, column=1,sticky=W)
@@ -191,30 +229,38 @@ button_next = Button(frame_grid, text="Next", command=lambda: [add_seitenzähler
 button_next.grid(row=16, column=3,sticky=W)
 button_next.grid_forget()
 
-myButton1 = Button(navigation, text="Unterricht", command=unterricht, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton1 = Button(unterricht_frame_navigation, text="Unterricht", command=unterricht, bg="#2d2d2d",fg="#e4bc1f")
+myButton1.place(x=0,y=0,width=rootWidth//4,height=rootHeight//4) #die anordnung
+myButton1.config(font=("Monteserrat",20))
 
+myButton2 = Button(schule_frame_navigation, text="Schule",command=schule, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton2.place(x=0,y=0,width=rootWidth//4,height=rootHeight//4)
+myButton2.config(font=("Monteserrat",20))
 
-myButton2 = Button(navigation, text="Schule",command=schule, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myButton3 = Button(persönliche_daten_frame_navigation, text="Persönliche Daten",command=persönliche_daten, bg="#2d2d2d",fg="#e4bc1f")
+myButton3.place(x=0,y=0,width=rootWidth//4,height=rootHeight//4)    #die Anordnung
+myButton3.config(font=("Monteserrat",20))
 
+myButton4 = Button(hauptmenü_frame_navigation, text="Hauptmenü", command=hauptmenü, bg="#2d2d2d", fg="#e4bc1f")         #Hauptmenü Button wurde erstellt
+myButton4.place(x=0,y=0,width=rootWidth//4,height=rootHeight//4)
+myButton4.config(font=("Monteserrat",20))
 
-myButton3 = Button(navigation, text="Persönliche Daten",command=persönliche_daten, bg="#2d2d2d",fg="#e4bc1f",width=70,height=10)
+myLehrerkürzel = Button(frame_inhalt_lehrerkürzel, text="Lehrerkürzel", command=lambda: lehrerkürzel(0) , bg="#2d2d2d", fg="#e4bc1f")
+myLehrerkürzel.place(x=0,y=0,width=rootWidth//2,height=65)
+myLehrerkürzel.config(font=("Monteserrat",20))
+myLehrerkürzel.place_forget()
 
+myInformation = Button(frame_inhalt_über_schule, text="Über die Schule", command=über_die_Schule, bg="#2d2d2d", fg="#e4bc1f")
+myInformation.place(x=0,y=0,width=rootWidth//2,height=65)
+myInformation.config(font=("Monteserrat",20))
+myInformation.place_forget()
 
-myButton4 = Button(navigation, text="Hauptmenü", command=hauptmenü, bg="#2d2d2d", fg="#e4bc1f", width=70, height=10)         #Hauptmenü Button wurde erstellt
+myEvent = Button(frame_inhalt_event, text="Event",command=event, bg="#2d2d2d",fg="#e4bc1f")                             #Eventbutton wurde erstellt
+myEvent.place(x=0,y=0,width=rootWidth//2,height=rootHeight//2+rootHeight//4)
+myEvent.config(font=("Monteserrat",20))
 
-
-myLehrerkürzel = Button(inhalt, text="Lehrerkürzel", command=lambda: lehrerkürzel(0) , bg="#2d2d2d", fg="#e4bc1f", width=(rootWidth//2), height=rootHeight-10)
-
-myLehrerkürzel.pack_forget()
-
-myInformation = Button(inhalt, text="Über die Schule", command=über_die_Schule, bg="#2d2d2d", fg="#e4bc1f", width=(rootWidth//2), height=rootHeight-10)
-
-myInformation.pack_forget()
-
-myEvent = Button(inhalt, text="Event",command=event, bg="#2d2d2d",fg="#e4bc1f",width=(rootWidth//2),height=65)                             #Eventbutton wurde erstellt
-
-
-myAusfall = Button(inhalt, text="Ausfall/Vertretung",command=ausfall, bg="#2d2d2d",fg="#e4bc1f",width=(rootWidth//2),height=roo)            #Ausfall/Vertretung Button wurde erstellt
-
+myAusfall = Button(frame_inhalt_ausfall, text="Ausfall/Vertretung",command=ausfall, bg="#2d2d2d",fg="#e4bc1f")            #Ausfall/Vertretung Button wurde erstellt
+myAusfall.place(x=0,y=0,width=rootWidth//2,height=rootHeight//2+rootHeight//4)
+myAusfall.config(font=("Monteserrat",20))
 
 root.mainloop()
